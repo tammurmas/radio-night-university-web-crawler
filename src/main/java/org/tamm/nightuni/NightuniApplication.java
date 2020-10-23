57ffa268b53674bb8af8d3c55871117b5972bf80
tammurmas <tammurmas@gmail.com>
2020-10-23 19:49:42 +0300

    application.properties

---
+++ src/main/java/org/tamm/nightuni/NightuniApplication.java
@@ -0,0 +1,13 @@
+package org.tamm.nightuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tamm/nightuni/NightuniApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tamm.nightuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
